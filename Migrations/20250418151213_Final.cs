d88e30a9d8da4f2b7737f99d837c9fdfcf6bd153
LevLaptop <luteranlevi@gmail.com>
2025-04-18 17:13:10 +0200

    Final Migration

---
+++ Migrations/20250418151213_Final.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CryptoWallet.Migrations
+{
+    /// <inheritdoc />
+    public partial class Final : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Balance",
+                table: "wallets",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Balance",
+                table: "wallets");
+        }
+    }
+}
